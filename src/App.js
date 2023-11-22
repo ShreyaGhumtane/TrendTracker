369fcdbba0ca0a38bd8d1c493d0d0c849ce28f4a
ShreyaGhumtane <149469608+ShreyaGhumtane@users.noreply.github.com>
2023-11-22 15:44:32 +0530

    Update code

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import Navbar from "./Components/Navbar";
 import React from 'react'
+import Navbar from './Components/Navbar'
+import News from './Components/News'
 
 export default function App() {
+  
   return (
     <div>
       <Navbar></Navbar>
+      <News></News>
     </div>
   )
 }
